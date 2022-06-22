99802ad9ffcb9a9c96228aafe5cbb05df7b0f2c2
cash <cash@cashwu.com>
2022-06-22 14:02:53 +0800

    Lab01 refactor test

--- js/test/dateUtility.test.js
+++ js/test/dateUtility.test.js
@@ -1,16 +1,27 @@
 import {DateUtility} from "../src/Lab01/dateUtility";
 
 describe("date utility", () => {
+    let dateUtility = new DateUtility();
+    let fake_getToday = jest.fn();
+
+    beforeEach(()=> {
+        dateUtility = new DateUtility();
+        fake_getToday = jest.fn();
+        dateUtility.getToday = fake_getToday;
+    })
 
     it("today is payday", () => {
-        
-        let dateUtility = new DateUtility();
-        let fake_getToday = jest.fn();
-        dateUtility.getToday = fake_getToday;
-        fake_getToday.mockReturnValueOnce(new Date(2020, 6, 5));
-
-        expect(dateUtility.isPayday()).toBe(true);
+        givenToday(5);
+        todayShouldBePayday();
     })
+
+    function todayShouldBePayday() {
+        expect(dateUtility.isPayday()).toBe(true);
+    }
+
+    function givenToday(date) {
+        fake_getToday.mockReturnValueOnce(new Date(2022, 6, date));
+    }
 
 });
 
