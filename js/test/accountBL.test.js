db2b6f8e23cbebfc0967cc24e07e30fd7d86e1ae
cash <cash@cashwu.com>
2022-06-22 14:49:43 +0800

    Lab02 login invalid should log

--- js/test/accountBL.test.js
+++ js/test/accountBL.test.js
@@ -5,22 +5,25 @@ describe("accountBL", () => {
     let accountBL = new AccountBL();
     let fake_getMember;
     let fake_getShaPassword;
-    
+    let fake_send;
+
     beforeEach(() => {
         accountBL = new AccountBL();
         fake_getMember = jest.fn();
         accountBL.getMember = fake_getMember;
         fake_getShaPassword = jest.fn();
         accountBL.getShaPassword = fake_getShaPassword;
+        fake_send = jest.fn();
+        accountBL.send = fake_send;
     })
 
     it("login is valid", () => {
         givenMember({
             "password": "sha-1234"
         });
-        
+
         givenShaPassword("sha-1234");
-        
+
         loginShouldBeValid("cash", "12345678");
     })
 
@@ -28,11 +31,24 @@ describe("accountBL", () => {
         givenMember({
             "password": "sha-1234"
         });
-       
+
         // 注意這裡也要改 !!
         givenShaPassword("sha-5678");
         loginShouldInvalid("cash", "wrong password");
     })
+
+    it("login invalid should log", () => {
+        givenLoginInvalid();
+       
+        // expect(fake_send.mock.calls[0][0]).toBe("cash login failed");
+        shouldLog("cash", "login failed");
+    })
+
+    function shouldLog(account, status) {
+        expect(fake_send.mock.calls[0][0]).toEqual(
+            expect.stringContaining(account) && expect.stringContaining(status)
+        )
+    }
 
     function givenMember(member) {
         fake_getMember.mockReturnValueOnce(member);
@@ -50,5 +66,16 @@ describe("accountBL", () => {
     function loginShouldInvalid(account, password) {
         let isValid = accountBL.login(account, password);
         expect(isValid).toBe(false)
+    }
+
+    function givenLoginInvalid() {
+        givenMember({
+            "password": "sha-1234"
+        });
+
+        // 注意這裡也要改 !!
+        givenShaPassword("sha-5678");
+
+        accountBL.login("cash", "wrong password");
     }
 })
