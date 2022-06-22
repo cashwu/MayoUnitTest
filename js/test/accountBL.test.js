3fd326db3a581ebb229db8d7b462de1745e5e077
cash <cash@cashwu.com>
2022-06-22 14:16:17 +0800

    Lab02 jest stub

--- js/test/accountBL.test.js
+++ js/test/accountBL.test.js
@@ -3,6 +3,20 @@ import {AccountBL} from "../src/Lab02/accountBL";
 describe("accountBL", () => {
    
     it("login is valid", () => {
-        expect(true);
+        
+        let accountBL = new AccountBL();
+        
+        let fake_getMember = jest.fn();
+        accountBL.getMember = fake_getMember;
+        fake_getMember.mockReturnValueOnce({ 
+            "password" : "sha-1234"
+        });
+
+        let fake_getShaPassword = jest.fn();
+        accountBL.getShaPassword = fake_getShaPassword;
+        fake_getShaPassword.mockReturnValueOnce("sha-1234");
+        
+        let isValid = accountBL.login("cash", "12345678");
+        expect(isValid).toBe(true)
     })
 })
