8fb587310df2b763d3d7cb32c78f3bfed0a345bd
cash <cash@cashwu.com>
2022-06-23 13:42:35 +0800

    Lab02 nsub stub

--- NetCore/Lab02/AccountBLTests.cs
+++ NetCore/Lab02/AccountBLTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Lab02.Model;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Lab02;
@@ -9,49 +10,19 @@ public class AccountBLTests
     [Test]
     public void Login_is_valid()
     {
-        var accountDao = new FakeAccountDao();
+        var accountDao = Substitute.For<IAccountDao>();
 
-        accountDao.Member = new Member
+        accountDao.GetMemberForLogin("cash").Returns(new Member
         {
             Password = "sha-1234"
-        };
+        });
 
-        var cryptography = new FakeCryptography();
-        cryptography.ShaPassword = "sha-1234";
+        var cryptography = Substitute.For<ICryptography>();
+        cryptography.CashSha("12345678").Returns("sha-1234");
 
         var accountBL = new AccountBL(accountDao, cryptography);
 
         var isValid = accountBL.Login("cash", "12345678");
         isValid.Should().BeTrue();
-    }
-}
-
-public class FakeCryptography : ICryptography
-{
-    public string ShaPassword { get; set; }
-
-    public string CashSha(string password)
-    {
-        return ShaPassword;
-    }
-}
-
-public class FakeAccountDao : IAccountDao
-{
-    public Member Member { get; set; }
-
-    public Member GetMemberForLogin(string account)
-    {
-        return Member;
-    }
-
-    public void SetLoginFailedCount(string account)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public int GetLoginFailedCount()
-    {
-        throw new System.NotImplementedException();
     }
 }
