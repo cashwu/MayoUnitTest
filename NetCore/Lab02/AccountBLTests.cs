ad5e22eec37b7295417dc3fd4b729696babcff91
cash <cash@cashwu.com>
2022-06-23 13:45:16 +0800

    Lab02 refactor

--- NetCore/Lab02/AccountBLTests.cs
+++ NetCore/Lab02/AccountBLTests.cs
@@ -7,22 +7,44 @@ namespace Lab02;
 
 public class AccountBLTests
 {
+    private IAccountDao _accountDao;
+    private ICryptography _cryptography;
+    private AccountBL _accountBL;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _cryptography = Substitute.For<ICryptography>();
+        _accountDao = Substitute.For<IAccountDao>();
+        _accountBL = new AccountBL(_accountDao, _cryptography);
+    }
+
     [Test]
     public void Login_is_valid()
     {
-        var accountDao = Substitute.For<IAccountDao>();
-
-        accountDao.GetMemberForLogin("cash").Returns(new Member
+        GivenMemberForLogin("cash", new Member
         {
             Password = "sha-1234"
         });
 
-        var cryptography = Substitute.For<ICryptography>();
-        cryptography.CashSha("12345678").Returns("sha-1234");
+        GivenShaPassword("12345678", "sha-1234");
 
-        var accountBL = new AccountBL(accountDao, cryptography);
+        LoginShouldBeValid("cash", "12345678");
+    }
 
-        var isValid = accountBL.Login("cash", "12345678");
+    private void LoginShouldBeValid(string account, string password)
+    {
+        var isValid = _accountBL.Login(account, password);
         isValid.Should().BeTrue();
+    }
+
+    private void GivenShaPassword(string password, string shaPassword)
+    {
+        _cryptography.CashSha(password).Returns(shaPassword);
+    }
+
+    private void GivenMemberForLogin(string account, Member member)
+    {
+        _accountDao.GetMemberForLogin(account).Returns(member);
     }
 }
