92e31c402bc1657970cbb6ab56c887bb58cc3a03
cash <cashwugeek@icloud.com>
2023-12-25 09:54:50 +0800

    Lab01 today is payday test - refactor

--- NetCore/Lab01/DateUtilityTests.cs
+++ NetCore/Lab01/DateUtilityTests.cs
@@ -6,13 +6,29 @@ namespace Lab01;
 
 public class DateUtilityTests
 {
+    private readonly FakeDateUtility _fakeDateUtility;
+
+    public DateUtilityTests()
+    {
+        _fakeDateUtility = new FakeDateUtility();
+    }
+
     [Fact]
     public void Today_is_Payday()
     {
         // test
-        var dateUtility = new FakeDateUtility();
-        dateUtility.Today = new DateTime(2023, 12, 5);
-        dateUtility.IsPayday().Should().BeTrue();
+        GivenToday(12, 5);
+        TodayShouldBePayday();
+    }
+
+    private void TodayShouldBePayday()
+    {
+        _fakeDateUtility.IsPayday().Should().BeTrue();
+    }
+
+    private void GivenToday(int month, int day)
+    {
+        _fakeDateUtility.Today = new DateTime(2023, month, day);
     }
 }
 
